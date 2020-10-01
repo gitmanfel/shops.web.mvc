9af72eff04e545049cf90e2be99e3a269e107bfe
Manzi Felicien <10253_Felicien_Manzi@technocite.onmicrosoft.com>
2020-10-01 10:52:49 +0200

    Ajoutez des fichiers projet.

---
+++ shops.web.mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace shops.web.mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
